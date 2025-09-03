0542c9ee1ae3d0bee35023273c21ef180225464c
Trinh Hung Quan <trinhhungquan12345@gmail.com>
2025-09-03 21:14:24 +0700

    First commit Navbar, Setting up Prisma

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import { AiFillBug } from "react-icons/ai"
+
+const NavBar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
+
+    const links = [
+        {label: 'Dashboard', href: '/'},
+        {label: 'Issues', href: '/issues'}
+    ];
+
+    return (
+        <>
+            <nav className="flex space-x-6 border-b border-zinc-400 mb-5 px-5 h-14 items-center">
+                <Link href="/"><AiFillBug /></Link>
+                <ul className="flex space-x-6">
+                    {links.map(link => 
+                        <Link key={link.href} className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`} href={link.href}>{link.label}</Link>
+                    )}           
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
