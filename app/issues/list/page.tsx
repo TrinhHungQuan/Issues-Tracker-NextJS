14f207344fcb6a7ea869630dd45514d859de814f
Trinh Hung Quan <trinhhungquan12345@gmail.com>
2025-09-09 17:15:49 +0700

    Remove duplicate skeletons

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,8 +1,8 @@
 import { prisma } from '@/prisma/client'
 import { Table } from '@radix-ui/themes'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import Link from '../components/Link'
 import IssueActions from './IssueActions'
+import Link from '@/app/components/Link';
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
